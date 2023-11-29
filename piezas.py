4bcdcee4b0076501384642b6654fe6ae59f2553d
AlejandroR <ajrodriguez.2628@gmail.com>
2023-11-29 02:02:34 -0400

    Clases para los sprites

---
+++ piezas.py
@@ -0,0 +1,14 @@
+import pygame
+from tablero import *
+
+SPRITES_DIR = r"Assets"
+ROOK_SPRITE = os.path.join(SPRITES_DIR, 'DarkRook-.png')
+
+class Rook(pygame.sprite.Sprite):
+    def __init__(self, coords, dimension):
+        super().__init__()
+        self.image = pygame.image.load(ROOK_SPRITE)
+        new_size = int(dimension * 0.95)
+        self.image = pygame.transform.scale(self.image, (new_size, new_size))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = coords
